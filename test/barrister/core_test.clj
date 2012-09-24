e2501cb0e110797988d0dc7a24eeae794575e60b
James Cooper <james@bitmechanic.com>
2012-09-24 06:34:10 -0700

    fix test namespace. add benchmark.

--- test/barrister/core_test.clj
+++ test/barrister/core_test.clj
@@ -1,8 +1,19 @@
-(ns barrister.core
+(ns barrister.core-test
   (:require [cheshire.core])
+  (:require [criterium.core])
   (:use [barrister.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:use [slingshot.slingshot :only [throw+]]))
 
-(deftest can-load-contract
-  (let [c (cheshire.core/parse-string (slurp "test/conform.json"))]
-    (is (= "34f6238ed03c6319017382e0fdc638a7" (:checksum (:meta c))))))
+(def c (cheshire.core/parse-string (slurp "test/conform.json")))
+(def h { "A.add" (fn [a b] (+ a b)) 
+         "A.calc" (fn [nums op]
+                    (throw+ {:type :rpc-err :code 30202 :message "blah.."})) })
+
+(deftest bench-invoke
+  (is (= 65 (get (invoke c h (create-rpc-req "A.add" [32 33])) "result")))
+  (criterium.core/quick-bench (invoke c h (create-rpc-req "A.add" [32 33]))))
+
+;;(deftest can-load-contract
+;;  (let [c (cheshire.core/parse-string (slurp "test/conform.json"))]
+;;    (is (= "34f6238ed03c6319017382e0fdc638a7" (:checksum (:meta c))))))
